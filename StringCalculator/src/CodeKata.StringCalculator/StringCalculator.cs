48248c58f4218774ddf168038862dc8722752d5c
Fati Iseni <fati.iseni@pozitrongroup.com>
2022-03-01 20:18:24 +0100

    Updated StringCalculator kata.

---
+++ StringCalculator/src/CodeKata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,85 @@
+﻿namespace CodeKata.StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers)) return 0;
+
+            var delimiters = new List<char>() { ',', '\n' };
+
+            if (numbers.StartsWith("//"))
+            {
+                var splitedInputByNewLine = numbers.Split('\n');
+                var newDelimiter = splitedInputByNewLine.First().Trim('/');
+                numbers = string.Join('\n', splitedInputByNewLine.Skip(1));
+
+                delimiters.Add(Convert.ToChar(newDelimiter));
+            }
+
+            var numberList = numbers.Split(delimiters.ToArray()).Select(s => int.Parse(s));
+
+            var negatives = numberList.Where(n => n < 0);
+
+            if (negatives.Any())
+            {
+                string negativeString = string.Join(',', negatives.Select(n => n.ToString()));
+                throw new Exception($"Negatives not allowed: {negativeString}");
+            }
+
+            var result = numberList.Where(x => x <= 1000).Sum();
+
+            return result;
+        }
+
+        public int Add_Original(string numbers)
+        {
+            List<int> negativeList = new List<int>();
+            string exMessage = "";
+            int currentNumber = 0, sum = 0;
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var delimiters = new List<char>() { ',', '\n' };
+            if (numbers.StartsWith("//"))
+            {
+                var splitedInputByNewLine = numbers.Split('\n');
+                var newDelimiter = splitedInputByNewLine.First().Trim('/');
+                numbers = String.Join('\n', splitedInputByNewLine.Skip(1));
+                delimiters.Add(Convert.ToChar(newDelimiter));
+            }
+            var splitedInput = numbers.Split(delimiters.ToArray());
+            foreach (var number in splitedInput)
+            {
+
+                if (int.TryParse(number, out currentNumber))
+                {
+                    if (currentNumber < 0)
+                    {
+                        if (string.IsNullOrEmpty(exMessage))
+                        {
+                            exMessage = "Negatives not allowed:";
+                            exMessage += " " + currentNumber.ToString();
+                        }
+                        else
+                        {
+                            exMessage += "," + currentNumber.ToString();
+                        }
+                        negativeList.Add(currentNumber);
+                    }
+                    if (currentNumber < 1000)
+                    {
+                        sum += currentNumber;
+                    }
+                }
+            }
+            if (negativeList.Count > 0)
+            {
+                throw new Exception(exMessage);
+            }
+            return sum;
+        }
+    }
+}
---
+++ StringCalculator/tests/CodeKata.StringCalculator.UnitTests/StringCalculator_Add.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xunit;
+
+namespace CodeKata.StringCalculator.UnitTests
+{
+    public class StringCalculator_Add
+    {
+        private StringCalculator _stringCalculator;
+        public StringCalculator_Add()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void ReturnsZero_GivenEmptyString()
+        {
+            var result = _stringCalculator.Add("");
+            Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("8", 8)]
+        public void ReturnsNumber_GivenStringWithOneNumber(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,3", 4)]
+        [InlineData("8,1", 9)]
+        public void ReturnsSum_GivenStringWithTwoCommaSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,3,4,5", 13)]
+        [InlineData("8,1,5,3,7", 24)]
+        public void ReturnsSum_GivenStringWithUnknownCommaSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("1\n2,8,4,4", 19)]
+        public void ReturnsSum_GivenStringWithCommaOrNewLineSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("1\n2,8,4,4", 19)]
+        public void ReturnsSum_GivenStringWithCustomDelimiter(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("-1,2", "Negatives not allowed: -1")]
+        [InlineData("2,-4,3,-5", "Negatives not allowed: -4,-5")]
+        public void Throws_GivenNegativeInput(string numbers, string expectedResult)
+        {
+            Action action = () => _stringCalculator.Add(numbers);
+
+            var ex = Assert.Throws<Exception>(action);
+            Assert.Equal(expectedResult, ex.Message);
+        }
+
+        [Theory]
+        [InlineData("1001,2", 2)]
+        public void ReturnsSum_GivenStringIgnoringValuesOver1000(string numbers, int expectedResult)
+        {
+            var result = _stringCalculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
